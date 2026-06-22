8e6724e1e9b932c48eac899594a9725e4e402b94
komolika07 <dagarekomolika2004@gmail.com>
2026-06-22 12:11:05 +0530

    User signup and login implemented

--- routes/listings.js
+++ routes/listings.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync");
 const {listingSchema} = require("../schema");
 const expressError = require("../utils/expressError");
 const Listing = require("../models/listing");
+const {isLoggedIn} = require("../middleware.js");
 
 const validateListing = (req, res, next) =>{
     let {error} = listingSchema.validate(req.body);
@@ -23,7 +24,7 @@ router.get("", wrapAsync( async (req, res) => {
 }));
 
 //get new listing form
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new.ejs");
 });
 
@@ -39,7 +40,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 }));
 
 //create new listing
-router.post("", validateListing, wrapAsync(async(req, res, next) => {
+router.post("", isLoggedIn, validateListing, wrapAsync(async(req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     req.flash("success", "New Listing Created!");
@@ -47,7 +48,7 @@ router.post("", validateListing, wrapAsync(async(req, res, next) => {
 }));
 
 //edit route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
@@ -58,7 +59,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //update route
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     req.flash("success", "Listing Updated!");
@@ -67,7 +68,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 }));
 
 //delete route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findByIdAndDelete(id);
     req.flash("success", "Listing Deleted!");
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const { saveRedirectUrl } = require('../middleware');
 
 
 router.get("/signup", (req, res) => {
@@ -14,14 +16,38 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const newUser = new User({ email, username });
         const registerUser = await User.register(newUser, password);
         console.log(registerUser);
-        req.flash("success", "user was registered");
-        res.redirect("/listings");
-    }catch(e){
+        req.login(registerUser, (err) => {
+            if (err) {
+                next(err);
+            }
+            req.flash("success", "Welcome to UrbanStay!");
+            res.redirect("/listings");
+        });
+
+    } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
     }
-    
-
 }));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true, }), async (req, res) => {
+    req.flash("success", "Welcome to urbanStay!! You are logged in.");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+});
+
+router.get("/logout", (req, res) => {
+    req.logOut((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "Logged you out");
+        res.redirect("/listings");
+    });
+})
 
 module.exports = router;
